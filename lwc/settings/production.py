ef4ec45de15faf6e252f4aed8f27b09eef22edb7
Mark Khazanov <markkhazanov@gmail.com>
2016-03-08 22:56:47 -0500

    add requirements

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -1,1 +1,28 @@
-production.py
+import os
+from django.conf import settings
+
+
+DEBUG = False
+TEMPLATE_DEBUG = True
+
+DATABASES = settings.DATABASES
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+
+# BASE_DIR= os.path.dirname(os.path.abspath(__file__))
+
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
